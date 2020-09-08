aac6372e578c3584fe7e46ee9110fec60fa50d76
caroMaio <carolinabelenmaio@gmail.com>
2020-09-07 21:55:59 -0300

    [ADD] btn close in navbar responsive, modified ICON, ADD IMG. Remove form-select

--- JS/app.js
+++ JS/app.js
@@ -15,14 +15,17 @@ writing('uSoft');
 
 const elementsNavbar = [
     document.querySelector("#listContent"),
-    document.querySelector("#listNavbar")
+    document.querySelector("#listNavbar"),
+    document.querySelector('#closeMenu')
 ];
 
 const $btnNavbar = document.querySelector('#btnNavbar');
 const titleSection = document.querySelector("#titleSection");
+const $open = document.querySelector('#openMenu');
 
 function showNavbar(){
     titleSection.classList.add('responsiveMoodNone');
+    $open.classList.add('responsiveMoodNone');
     elementsNavbar.forEach(element => {
         element.classList.remove('responsiveMoodNone');
     })
@@ -30,6 +33,7 @@ function showNavbar(){
 
 function hideNavbar(){
     titleSection.classList.remove('responsiveMoodNone');
+    $open.classList.remove('responsiveMoodNone')
     elementsNavbar.forEach(element => {
         element.classList.add('responsiveMoodNone');
     });
